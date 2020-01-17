298f9015bd606c0f1bb2afcfc24b7b1b0bdc356a
eduardoffelipe <eduardoftoledo70@gmail.com>
2020-01-17 18:05:29 -0300

    Começo da base da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes')
+
+mongoose.connect('mongodb+srv://eduardoffelipe:eduardo..@cluster0-kwg0k.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const app = express();
+
+app.use(express.json())
+app.use(routes);
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+//Tipos de parâmetros: 
+//Query Params: req.query (Filtros, ordenação, paginação, ...) - GET
+//Route Params:resquest.params (Identificar umn recurso na alteracao ou remocao)
+//Body: request.body (Dados para a criação ou alteração de um registro)
+
+// MongoDB (Não-relacional)
+
+
+
+app.listen(3333);
