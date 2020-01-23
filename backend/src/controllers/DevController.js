67e3b002d7fcf1f088c9926219d993a87679b2c9
eduardoffelipe <eduardoftoledo70@gmail.com>
2020-01-22 22:57:10 -0300

    CRUD de Dev atualizado com método de Apagar e Atualizar

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -29,19 +29,49 @@ module.exports = {
             coordinates: [longitude, latitude],
         }
     
-        dev = await Dev.create({
-            github_username,
-            name,
-            avatar_url,
-            bio,
-            techs: techsArray,
-            location,
-        })
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location,
+            })
     
-        // Filtrar as conexões que estão há no máximo 10km de distância
-        // e que o novo dev tenha pelo menos uma das tecnologias filtradas
-    }
+            // Filtrar as conexões que estão há no máximo 10km de distância
+            // e que o novo dev tenha pelo menos uma das tecnologias filtradas
+        }
+    
+            return response.json(dev);
+    },
+    async update(request, response) {
+        const { id } = request.params;
+        let { techs, latitude, longitude, ...dataToUpdate } = request.body;
+        
+        if (dataToUpdate.github_username) 
+          delete dataToUpdate.github_username 
+    
+        if (longitude && latitude) 
+          dataToUpdate.location = {
+            type: "Point",
+            coordinates: [longitude, latitude]
+          };
+    
+        if (techs)
+          dataToUpdate.techs = parseStringAsArray(techs);
+    
+        const dev = await Dev.findByIdAndUpdate(id, dataToUpdate, { new: true });
     
         return response.json(dev);
-},
+      },
+    
+      async destroy(request, response) {
+        const { id } = request.params;
+    
+        await Dev.findByIdAndDelete(id);
+    
+        return response.json({ message: "Apagado com sucesso" });
+      }
+
+
 };
